3ee0945a29d9d562c8408c01d370f0def25002a2
arlengrace <63443490+arlengrace@users.noreply.github.com>
2020-07-25 15:26:33 -0700

    feat: disable scrolling in gallery fs

--- infinitylabs.dev/gallery/scripts.js
+++ infinitylabs.dev/gallery/scripts.js
@@ -1,18 +1,22 @@
 function open_img(img) {
     const fs = document.getElementById('fullscreen_container');
+    const body = document.getElementsByTagName('body')[0];
     fs.style.display = "block";
+    body.style.overflow = 'hidden';
     document.getElementById('fullscreen_img').src=`../images/gallery/${img}.JPG`;
 }
 
 function close_img() {
     const fs = document.getElementById('fullscreen_container');
+    const body = document.getElementsByTagName('body')[0];
     fs.style.display = "none";
+    body.style.overflow = 'visible';
 }
 
 document.onkeydown = function(event) {
     const fs = document.getElementById('fullscreen_container');
     event = event || window.event;
     if ((event.key === 'Escape' || event.key === 'Esc' || event.keyCode === 27) && fs.style.display == "block") {
-        fs.style.display = "none";
+        close_img();
     }
 }
